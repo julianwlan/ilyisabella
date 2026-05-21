f7dc6ecd517b813ebf2de5701f078213a6ec9ae8
Julian Breitfuß <brejud24@htl-kaindorf.at>
2026-05-21 16:52:22 +0200

    uv-banner http, https error fixed

--- js/script.js
+++ js/script.js
@@ -37,7 +37,7 @@ navigator.geolocation.getCurrentPosition(async (pos) => {
 
     const cityName = data.address.suburb;
 
-    const weather = await fetchData(`http://api.weatherapi.com/v1/forecast.json?days=4&key=ff76f32b6d9940a9b2674941262005&lang
+    const weather = await fetchData(`https://api.weatherapi.com/v1/forecast.json?days=4&key=ff76f32b6d9940a9b2674941262005&lang
     =en&q=${latitude},${longitude}`);
 
     console.log(weather);
